2dbe7651afbbb1ec94c839cf3cbe6fb587106962
AnushreeSan <119234199+AnushreeSan@users.noreply.github.com>
2023-05-15 21:04:23 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var bg,bgImg;
+var player1, player2, player3, player4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
